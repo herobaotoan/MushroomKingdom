fa82492780b4ab16c774b103a9707b0c61569466
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2023-08-02 17:46:56 +0700

    Update KingdomsView.swift

--- MushroomKingdom/KingdomsView.swift
+++ MushroomKingdom/KingdomsView.swift
@@ -1,21 +1,54 @@
-//
-//  KingdomsView.swift
-//  MushroomKingdom
-//
-//  Created by Toan Tran Chi on 02/08/2023.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 1
+  Author: Tran Chi Toan
+  ID: s3891637
+  Created  date: 02/08/2023
+  Last modified: 06/08/2023
+  Acknowledgement: https://github.com/TomHuynhSG/SSET-Contact-List-iOS
+*/
 
 import SwiftUI
 
 struct KingdomsView: View {
+    @State var active: Bool = true
     var body: some View {
-        HStack {
-            ZStack{
+        ZStack {
+            if isWelcomeActive {
                 Image("background")
+                VStack {
+                    HStack {
+                        Image("logo")
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color(.red), lineWidth: 6))
+                        Text("Mushroom Kingdom")
+                            .font(.system(size: 40))
+                            .bold()
+                    }
+                    Text("MAMMA MIA!")
+                        .font(.system(size: 80))
+                        .bold()
+                    Button(action: {
+                        active = false
+                    }, label: {
+                        Capsule()
+                        .fill(Color.red.opacity(0.2))
+                        .padding(8)
+                        .frame(height:80)
+                        .overlay(Text("Get Started")
+                            .font(.system(.title3, design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white))
+                            //TODO: Change color
+                    })
+                }
+            } else {
+                ContentView()
             }
-            ZStack{
-                Image("background")
-            }
+            
         }
     }
 }
