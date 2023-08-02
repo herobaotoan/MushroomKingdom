61919252f80d4519cc4f2dfbfbb595f69e12ceab
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2023-08-02 17:46:37 +0700

    Update ContentView.swift

--- MushroomKingdom/ContentView.swift
+++ MushroomKingdom/ContentView.swift
@@ -1,9 +1,14 @@
-//
-//  ContentView.swift
-//  MushroomKingdom
-//
-//  Created by Toan Tran Chi on 26/07/2023.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2023B
+  Assessment: Assignment 1
+  Author: Tran Chi Toan
+  ID: s3891637
+  Created  date: 19/07/2023
+  Last modified: 06/08/2023
+  Acknowledgement: https://github.com/TomHuynhSG/SSET-Contact-List-iOS
+*/
 
 import SwiftUI
 
